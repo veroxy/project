42de1c99089c9d1261bb99e699287ede2b6fcd85
bob walker <bob@randomness.org.uk>
2013-01-11 21:14:19 +0000

    protect against spaces in project name

--- project.sh
+++ project.sh
@@ -10,13 +10,13 @@ PROJECTNAME=$1
 PROJECTDIR="$HOME/Documents/$PROJECTNAME"
 
 # Creates directory with desired project name
-mkdir -p $PROJECTDIR
+mkdir -p "$PROJECTDIR"
 
 # Enters directory
 
 # Clones Steve Rydz's boilerplate directory
-git clone git@github.com:steverydz/boilerplate.git $PROJECTDIR
+git clone git@github.com:steverydz/boilerplate.git "$PROJECTDIR"
 
 
 # Removes readme and git
-rm -rf $PROJECTDIR/{.git,README.md}
+rm -rf "$PROJECTDIR/{.git,README.md}"
